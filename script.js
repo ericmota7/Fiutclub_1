78891add2e1177acf143f706c2d1b8f4a5f18983
Lucas Costa <141447167+lucasr099@users.noreply.github.com>
2024-11-03 13:57:36 -0300

    atu

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// Aguarda o carregamento completo da página
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleciona todas as imagens que você deseja mover
     const movableImages = document.querySelectorAll('.movable-image');
 
-    // Função para atualizar a posição das imagens ao rolar
     function updateImagePositions() {
         const scrollPosition = window.scrollY;
 
         movableImages.forEach(image => {
-            // Define a nova posição vertical com base na posição de rolagem
-            const movementFactor = 0.5; // Ajuste este valor para controlar a velocidade do movimento
+            const movementFactor = 0.5; 
             const newY = scrollPosition * movementFactor;
             image.style.transform = `translateY(${newY}px)`;
         });
     }
 
-    // Adiciona o evento de rolagem
     window.addEventListener('scroll', updateImagePositions);
 });
